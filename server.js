559e7bdc6c3108b6e9890f3400b49b067cee9076
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-10 15:23:15 -0800

    try app.get *

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ if (process.env.NODE_ENV !== 'production') {
     const webpackMiddleware = require('webpack-dev-middleware');
     const webpack = require('webpack');
     const webpackConfig = require('./webpack.config.js');
-
     app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
     app.use(express.static('dist'));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'dist', 'index.html'))
     })
 }
